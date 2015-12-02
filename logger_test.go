e3e385d7a1dacd8d28a617e1a86c776a64651d50
Cyril David <cyril@heroku.com>
2015-12-02 01:50:42 -0800

    Make the test work / more complete

--- logger_test.go
+++ logger_test.go
@@ -1,18 +1,57 @@
 package ln
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
 
-func ExampleSimpleError() {
+func TestSimpleError(t *testing.T) {
+	out := bytes.Buffer{}
+	oldFilters := DefaultLogger.Filters
+	DefaultLogger.Filters = []Filter{NewWriterFilter(&out, nil)}
+	defer func() {
+		DefaultLogger.Filters = oldFilters
+	}()
 	Info(F{"err": fmt.Errorf("This is an Error!!!")}, "fooey", F{"bar": "foo"})
-	// Output: Just show me the output
+	data := []string{
+		`err="This is an Error!!!"`,
+		`fooey`,
+		`bar=foo`,
+	}
+
+	for _, line := range data {
+		if !bytes.Contains(out.Bytes(), []byte(line)) {
+			t.Fatalf("Bytes: %s not in %s", line, out.Bytes())
+		}
+	}
 }
 
-func ExampleDebug() {
+func TestDebug(t *testing.T) {
 	oldPri := DefaultLogger.Pri
 	defer func() { DefaultLogger.Pri = oldPri }()
+
+	out := bytes.Buffer{}
+	oldFilters := DefaultLogger.Filters
+	DefaultLogger.Filters = []Filter{NewWriterFilter(&out, nil)}
+	defer func() {
+		DefaultLogger.Filters = oldFilters
+	}()
 
 	// set priority to Debug
 	DefaultLogger.Pri = PriDebug
 	Debug(F{"err": fmt.Errorf("This is an Error!!!")})
-	// Output: Just show me the output
+
+	data := []string{
+		`err="This is an Error!!!"`,
+		`_lineno=`,
+		`_function=ln.TestDebug`,
+		`_filename=github.com/apg/ln/logger_test.go`,
+	}
+
+	for _, line := range data {
+		if !bytes.Contains(out.Bytes(), []byte(line)) {
+			t.Fatalf("Bytes: %s not in %s", line, out.Bytes())
+		}
+	}
 }
