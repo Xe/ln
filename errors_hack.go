d54da995c4bb59129980ccf99ca1439f826651ad
Christine Dodrill <me@christine.website>
2019-02-02 00:09:29 +0000

    gofmt

--- errors_hack.go
+++ errors_hack.go
@@ -1,22 +1,21 @@
 package ln
 
 type wrapper interface {
-  Unwrap() error
+	Unwrap() error
 }
 
 func unwrap(err error) error {
-  u, ok := err.(wrapper)
-  if !ok {
-    return nil
-  }
-  
-  return u.Unwrap()
-}
+	u, ok := err.(wrapper)
+	if !ok {
+		return nil
+	}
 
+	return u.Unwrap()
+}
 
 func doGoError(err error, f F) {
 	uw := unwrap(err)
 	if uw != nil {
-		f["wrapped_err"] = uw	
+		f["wrapped_err"] = uw
 	}
 }
